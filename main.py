1558bf94ccce0267574dddb36c7ac2a6ecb14e03
Андронов Олег <gennadych-65@yandex.ru>
2025-01-17 15:32:51 +0300

    Modification class_Fighter
    add  change_weapon

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 # Фиксируем исходные данные задачи
 
 # Класс, представляющий бойца
+# class Fighter:
+#     def __init__(self, name):
+#         self.name = name            # Имя бойца
+#
+#     def attack(self, monster):      # Метод атаки монстра
+#         print(f"{self.name} атакует монстра!")
+
+# Класс Fighter после его модифицирования
 class Fighter:
     def __init__(self, name):
-        self.name = name            # Имя бойца
+        self.name = name                    # Имя бойца
+        self.weapon = None
 
-    def attack(self, monster):      # Метод атаки монстра
-        print(f"{self.name} атакует монстра!")
+    def change_weapon(self, new_weapon):    # Метод для выбора/смены оружия
+        self.weapon = new_weapon
+        print(f"{self.name} выбрал {self.weapon.__class__.__name__}.")
+
+    def attack(self, monster):              # Метод атаки бойцом монстра, используя выбранное оружие
+        if self.weapon is None:             # Проверка наличия оружия
+            print(f"{self.name} не имеет оружия.")
+        else:
+            print(f"{self.name} атакует {monster.name}!")
+            damage = self.weapon.attack()   # Вызывается метод атаки у объекта оружия
+            monster.take_damage(damage)     # Cохранение урона от использованного оружия
 
 # Класс, представляющий монстра
 
@@ -35,3 +53,17 @@ class Weapon(ABC):
     @abstractmethod
     def attack(self):               # Метод атаки для реализации в каждом конкретном типе оружия
         pass
+
+# Создание производных классов, унаследованных от Weapon
+
+# Класс Sword (меч)
+class Sword(Weapon):
+    def attack(self):
+        print("Боец наносит удар мечом!")
+        return 50                   # Уникальность метода attack в размере урона, который наносит меч
+
+# Класс Bow (лук)
+class Bow(Weapon):
+    def attack(self):
+        print("Боец выпускает стрелу из лука!")
+        return 30                   # Уникальность метода attack в размере урона, который наносит лук
